5a5dcff6f1a885e255f8e53c91ae7b19970e21fe
somdyutibiswas <somdyutibiswas25@gmail.com>
2025-07-19 11:28:03 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(){
     printf("Booting graphics library software\n");
     application_init_info init_info;
     init_info.app_name = "GLSOFT";
-    init_info.width = 1200;
+    init_info.width = 800;
     init_info.height = 800;
     if (!application_init(init_info)) return 1;
 
